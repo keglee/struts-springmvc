dfcd123793b45ca56197061495c1ba280437a96b
iversonx <15118048183@163.com>
2020-01-09 10:24:09 +0800

    采用注解方式，将spring和struts2集成

--- src/main/java/com/iversonx/struts_springmvc/action/UserAction.java
+++ src/main/java/com/iversonx/struts_springmvc/action/UserAction.java
@@ -1,13 +1,22 @@
 package com.iversonx.struts_springmvc.action;
 
+import com.iversonx.struts_springmvc.service.UserService;
 import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class UserAction extends ActionSupport {
+
+    @Autowired
+    private UserService userService;
+
     private static final long serialVersionUID = -1353901915599323577L;
     private String username;
     private String password;
 
     public String show() {
+        username = userService.show();
         return "success";
     }
 
---
+++ src/main/java/com/iversonx/struts_springmvc/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.iversonx.struts_springmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Lijie
+ * @version 1.0
+ * @date 2020/1/9 9:48
+ */
+@Configuration
+@ComponentScan("com.iversonx.struts_springmvc")
+public class ApplicationConfig {
+}
