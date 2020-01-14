80587f157e0843e42d2558221380ee83aa418120
iversonx <15118048183@163.com>
2020-01-14 22:21:10 +0800

    参数解析 - url参数

--- src/main/java/com/iversonx/struts_springmvc/extend/ActionHandlerInterceptor.java
+++ src/main/java/com/iversonx/struts_springmvc/extend/ActionHandlerInterceptor.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -29,8 +33,6 @@ public class ActionHandlerInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Enumeration<String> headerNames = request.getHeaderNames();
-        String contentType = request.getContentType();
-
         System.out.println("Request URL: " + request.getRequestURL());
         System.out.println("Request Method: " + request.getMethod());
         System.out.println("Request Headers: ");
@@ -46,11 +48,13 @@ public class ActionHandlerInterceptor extends HandlerInterceptorAdapter {
             // 当嵌套属性为null时，自动创建嵌套属性的实例
             bw.setAutoGrowNestedPaths(true);
 
+            String contentType = request.getContentType();
+            Enumeration<String> names = request.getParameterNames();
             if(contentType == null
                     || "".equals(contentType)
                     || MediaType.APPLICATION_FORM_URLENCODED_VALUE.equals(contentType)) {
                 // 从查询字符串或表单数据(content-type=application/x-www-form-urlencoded)获取参数
-                Enumeration<String> names = request.getParameterNames();
+                names = request.getParameterNames();
                 while (names.hasMoreElements()) {
                     String name = names.nextElement();
                     String value = request.getParameter(name);
@@ -60,6 +64,11 @@ public class ActionHandlerInterceptor extends HandlerInterceptorAdapter {
                 }
             } else {
                 // 其他类型
+                MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+                HttpInputMessage httpInputMessage = new ServletServerHttpRequest(request);
+                Object object = messageConverter.read(handlerMethod.getBean().getClass(), httpInputMessage);
+                BeanUtils.copyProperties(object, handlerMethod.getBean());
+                System.out.println(object);
             }
         }
         return true;
